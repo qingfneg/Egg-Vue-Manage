3c412ae032552dc395a163ced25ea14e6eb43fb6
qingfeng <601672784@qq.com>
2021-07-22 16:18:04 +0800

    发送邮件验证码

--- app/controller/util.ts
+++ app/controller/util.ts
@@ -1,43 +1,38 @@
 import { Controller } from 'egg';
-import svgCaptcha = require('svg-captcha');
+const nodemailer = require('nodemailer');
 export default class UtilController extends Controller {
-  public async verifyImageCode() {
-    const { ctx } = this;
-    // 1.取出服务端中保存的验证码和过期时间
-    const serverCaptcha = ctx.session.captcha;
-    const serverCode = serverCaptcha.code;
-    const serverExpire = serverCaptcha.expire;
-    // 2.获取客户端传递过来的验证码
-    const { clientCode } = ctx.query;
-    console.log(serverCode, serverExpire, clientCode);// http://127.0.0.1:7001/verify?clientCode=1dee
-    if (Date.now() > serverExpire) {
-      ctx.body = '验证码已过期';
-    } else if (serverCode !== clientCode) {
-      ctx.body = '验证码不正确';
-    } else {
-      ctx.body = '验证通过';
-    }
-  }
+
   public async imageCode() {
     const { ctx } = this;
-    // 1.生成验证码
-    const c = svgCaptcha.create({
-      size: 4, // 验证码长度
-      width: 160, // 验证码图片宽度
-      height: 60, // 验证码图片高度
-      fontSize: 50, // 验证码文字大小
-      ignoreChars: '0oO1ilI', // 验证码字符中排除内容 0o1i
-      noise: 4, // 干扰线条的数量
-      color: true, // 验证码的字符是否有颜色，默认没有，如果设定了背景，则默认有
-      background: '#eee', // 验证码图片背景颜色
+    ctx.body = ctx.helper.createImageCode();
+  }
+  public async emailCode() {
+    // 1.创建发送者对象
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.qq.com',
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: '601672784@qq.com', // 发送邮件的邮箱
+        pass: 'pcwwgmizkfibbcig', // 邮箱对应的授权码
+      },
     });
-    // console.log(c); // {text: 验证码字符串, data: 验证码svg图片}
-    // 2.保存生成的验证码
-    ctx.session.captcha = {
-      code: c.text,
-      expire: Date.now() + 60 * 1000, // 验证码1分钟之后过期
+    // 2.创建需要发送的内容
+    const code = Math.random().toString(16).slice(2, 6)
+      .toUpperCase();
+    const info = {
+      from: '601672784@qq.com', // 谁发的
+      to: '18889796429@163.com', // 发给谁
+      subject: '知播渔管理后台注册验证码', // 邮件标题
+      text: `您正在注册知播渔管理后台系统，您的验证码是${code}`, // 邮件内容
     };
-    // 3.将验证码发送给客户端
-    ctx.body = c.data;
+    // 3.发送邮件
+    transporter.sendMail(info, (err, data) => {
+      if (err) {
+        console.log('发送邮件失败：', err);
+      } else {
+        console.log('发送邮件成功:', data);
+      }
+    });
   }
 }
